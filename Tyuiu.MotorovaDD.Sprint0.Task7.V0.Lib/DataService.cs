e04f50f4291b801ac5204effd6d1f6f1834ed815
Tyuiu.MotorovaDD@outlook.com <Tyuiu.MotorovaDD@outlook.com>
2023-10-01 23:41:37 +0500

    Task7

---
+++ Tyuiu.MotorovaDD.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Tyuiu.MotorovaDD.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+    {
+        int[] resultArray = new int[5];
+        for (var i=0; i<resultArray.Lenght; i++)
+       {
+         resultArray[i]= numOne[i] + numTwo[i]; 
+       }
+     }
+            return resultArray;
+}
+
---
+++ Tyuiu.MotorovaDD.Sprint0.Task7.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MotorovaDD.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Tyuiu.MotorovaDD.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a5178ca1-c2c6-4259-b921-12514aca9220")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
